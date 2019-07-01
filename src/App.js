d171e1fb10918b01c6b2fee4a549b3583d26a924
nonsodaniel <nonsodaniel07@gmail.com>
2019-07-01 12:02:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>hello</div>
+    );
+  }
+}
+
+export default App;
