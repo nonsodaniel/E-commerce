73e18ace65e9c005e20a1d458e9fccfae8dbf8e7
nonsodaniel <nonsodaniel07@gmail.com>
2019-07-01 12:13:22 +0100

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import Navbar from './components/layouts/Navbar'
 
 class App extends Component {
   state = {};
 
   render() {
     return (
-      <div>hello</div>
+      <Navbar />
     );
   }
 }
